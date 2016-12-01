697ac8c38b712c3e5b3ebaa677ce89fb411948f8
wenbojx <wenbojx@sina.com>
2016-12-01 07:00:17 +0000

    m

--- web/note/config/web.php
+++ web/note/config/web.php
@@ -12,14 +12,6 @@ $config = [
             'cookieValidationKey' => 'yifang*$#yifang',
             'enableCookieValidation' => false,
         ],
-        /*
-        'redis' => [
-            'class' => 'yii\redis\Connection',
-            'hostname' => '127.0.0.1',
-            'port' => 6379,
-            'database' => 0,
-        ],
-        */
         'cache' => [
             'class' => 'yii\redis\Cache',
              'redis' => [
