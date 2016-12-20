b9289c5931c3c2507584bfc296b224343a7dc9a9
wenbojx <wenbojx@sina.com>
2016-12-20 14:55:50 +0000

    m

--- notebook/renderer-process/book/book.js
+++ notebook/renderer-process/book/book.js
@@ -9,10 +9,38 @@ function callBackGetBookInfo(datas){
 }
 
 ipcRenderer.on('getChapterList', function(event, datas) {
-	console.log(datas);
 	callBackGetChapterList(datas);
 });
 function callBackGetChapterList(datas){
+	//console.log(datas.length);
+	var chapterDatas = sortChaterDatas(datas);
 	
+}
+function sortChaterDatas(datas){
+	var sortData = new Array();
+	var j = 0;
+	for (var i = 0; i < datas.length; i++) {
+		if (datas[i]['fid'] == 0) {
+			//console.log(datas[i]);
+			var id = datas[i]['id'];
+			sortData[id] = new Array();
+			sortData[id]['node'] = datas[i];
+			sortData[id]['child'] = new Array();
+			//datas.splice(i, 1);
+			j++;
+		}
+	}
+	j = 0;
+	for (var i = 0; i < datas.length; i++) {
+		var fid = datas[i]['fid'];
+		if (fid != 0 && typeof(sortData[fid]) != "undefined") {
+
+			sortData[fid]['child'][j] = datas[i];
+			//datas.splice(i, 1);
+			j++;
+		}
+	}
+	console.log(sortData);
+	return sortData;
 }
 
